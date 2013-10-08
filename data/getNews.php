485a4fed0f2a0b6a1e142670a3149992fdb42664
TitouanR <titouan.rossier@gmail.com>
2013-10-08 17:53:14 -0400

    Connection to BDD in a separate file, getNews working

--- data/getNews.php
+++ data/getNews.php
@@ -1,22 +1,23 @@
 <?php
 
+include("connection.php");
 print <<<END
 <table>
 	<tr>
 								<th>Nom</th>
 								<th>Description</th>
+                                <th>Date</th>
 							</tr>
 END;
 
-include('connection.php');
-
-$getNewsQuery = sprintf("SELECT title, content, date FROM news ORDER BY date DESC");
+$getNewsQuery = "SELECT title, content, date FROM news ORDER BY date DESC";
 $getNewsResult = mysql_query($getNewsQuery);
+
 
 while($newsRow = mysql_fetch_array($getNewsResult)){
 	echo "<tr>";
-	echo "<td width=\"100\"=>".$newsRow[0]."</td>";
-	echo "<td>".$newsRow[1]."</td>";
+	echo "<td>".utf8_encode($newsRow[0])."</td>";
+	echo "<td>".utf8_encode($newsRow[1])."</td>";
 	echo "<td>".$newsRow[2]."</td>";
 	echo "<td><button type=\"button\" class=\"btn btn-primary\">Editer</button></td>";
 	echo "</tr>";
