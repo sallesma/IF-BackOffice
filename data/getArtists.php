58ba6698745698c19d611cd5d4ccfa538a754b7d
Martin Salles <salles.martin@gmail.com>
2013-10-08 17:56:51 -0400

    getArtist uses db data

--- data/getArtists.php
+++ data/getArtists.php
@@ -1,44 +1,32 @@
 <?php
 
 print <<<END
-<tr>
-							<th>Nom</th>
-							<th>Style</th>
-							<th>Programmation</th>
-						</tr>
-						<tr>
-							<td>Alain Souchon</td>
-							<td>Chanson française</td>
-							<td>Vendredi aprem</td>
-							<td>
-								<button type="button" class="btn btn-primary">Editer</button>
-							</td>
-						</tr>
-						<tr>
-							<td>Alain Souchon</td>
-							<td>Chanson française</td>
-							<td>Vendredi aprem</td>
-							<td>
-								<button type="button" class="btn btn-primary">Editer</button>
-							</td>
-						</tr>
-						<tr>
-							<td>Alain Souchon</td>
-							<td>Chanson française</td>
-							<td>Vendredi aprem</td>
-							<td>
-								<button type="button" class="btn btn-primary">Editer</button>
-							</td>
-						</tr>
-						<tr>
-							<td>Alain Souchon</td>
-							<td>Chanson française</td>
-							<td>Vendredi aprem</td>
-							<td>
-								<button type="button" class="btn btn-primary">Editer</button>
-							</td>
-						</tr>
-                        
+<table>
+	<tr>
+								<th>Nom</th>
+								<th>Genre</th>
+								<th>Scène</th>
+								<th>Jour</th>
+								<th>Heure</th>
+							</tr>
 END;
+
+include('connection.php');
+
+$getArtistsQuery = sprintf("SELECT nom, genre, jour, scene, debut FROM artists ORDER BY nom");
+$getArtistsResult = mysql_query($getArtistsQuery);
+
+while($artistRow = mysql_fetch_assoc($getArtistsResult)){
+	echo "<tr>";
+	echo "<td width=\"100\">".$artistRow['nom']."</td>";
+	echo "<td>".$artistRow['genre']."</td>";
+	echo "<td>".$artistRow['scene']."</td>";
+	echo "<td>".$artistRow['jour']."</td>";
+	echo "<td>".$artistRow['debut']."</td>";
+	echo "<td><button type=\"button\" class=\"btn btn-primary\">Plus</button></td>";
+	echo "</tr>";
+}
+
+echo "</table>";
 
 ?>
