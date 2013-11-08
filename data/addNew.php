0ba16779853b2cbe86300a453dc25bebee4358fe
Martin Salles <salles.martin@gmail.com>
2013-11-08 16:39:59 -0500

    deleted useless utf8_decode

--- data/addNew.php
+++ data/addNew.php
@@ -3,8 +3,8 @@
 include ("connection.php");
 
 
-$title = utf8_decode ($_POST['title']);
-$body =  utf8_decode($_POST['body']);
+$title = $_POST['title'];
+$body = $_POST['body'];
 
 $addNewsQuery ="INSERT INTO news(title, content) VALUES ('".$title."', '".$body."')";
 
