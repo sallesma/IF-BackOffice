57d25b7a4587ab26e46d6a1bad1ac976c41d2c9f
Martin Salles <salles.martin@gmail.com>
2013-11-19 15:55:13 -0500

    deleted tables data in base.sql

--- base.sql
+++ base.sql
@@ -44,14 +44,6 @@ CREATE TABLE IF NOT EXISTS `artists` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=24 ;
 
---
--- Contenu de la table `artists`
---
-
-INSERT INTO `artists` (`id`, `name`, `picture`, `style`, `description`, `day`, `stage`, `beginHour`, `endHour`, `website`, `facebook`, `twitter`, `youtube`) VALUES
-(22, 'Daft Punk', 'http://titouanrossier.com/ifM/data/fileUpload/artists/tumblr_mm35b87dGz1qmwrnuo1_1280.jpg', '', 'Daft Punk est un groupe franÃ§ais de musique Ã©lectronique, originaire de Paris. Actifs depuis 1993, Thomas Bangalter et Guy-Manuel de Homem-Christo, les deux membres, ont alliÃ© Ã  leurs sons electro, house et techno des tonalitÃ©s rock, groove et disco. Le groupe participa Ã  la crÃ©ation et la dÃ©mocratisation du mouvement de musique Ã©lectronique baptisÃ© French touch. Ils font partie des artistes franÃ§ais s''exportant le mieux Ã  l''Ã©tranger, et ce depuis la sortie de leur premier vÃ©ritable succÃ¨s, Da Funk en 1996. Une des originalitÃ©s des Daft Punk est la culture de leur notoriÃ©tÃ© d''artistes indÃ©pendants anonymes, Ã  l''aide de casques et de costumes.', 'vendredi', 'principale', '23:15:00', '01:15:00', 'http://www.daftpunk.com/', 'https://www.facebook.com/daftpunk', 'https://twitter.com/daftpunk', 'http://www.youtube.com/user/DaftPunkVEVO'),
-(23, 'Titouan Rossier', 'http://titouanrossier.com/ifM/data/fileUpload/artists/1476307_10151857078773893_1899618758_n.jpg', '', 'Un type chevelu', 'vendredi', 'principale', '10:15:00', '10:15:00', 'titouanrossier.com', '', '', '');
-
 -- --------------------------------------------------------
 
 --
@@ -63,14 +55,6 @@ CREATE TABLE IF NOT EXISTS `filters` (
   `url` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
-
---
--- Contenu de la table `filters`
---
-
-INSERT INTO `filters` (`id`, `url`) VALUES
-(22, 'http://titouanrossier.com/ifM/data/fileUpload/filters/martine.jpg'),
-(23, 'http://titouanrossier.com/ifM/data/fileUpload/filters/martine2.jpg');
 
 -- --------------------------------------------------------
 
@@ -88,26 +72,6 @@ CREATE TABLE IF NOT EXISTS `infos` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=93 ;
 
---
--- Contenu de la table `infos`
---
-
-INSERT INTO `infos` (`id`, `name`, `picture`, `isCategory`, `content`, `parent`) VALUES
-(75, 'Bar Ã©tuville', '', 0, 'Ah ah ah', 70),
-(72, 'Official Cauette', '', 0, 'Chez JP', 70),
-(73, 'Bar Ã  vin', '', 0, 'Glou glou glou', 70),
-(70, 'Bars', '', 1, '', 0),
-(71, 'Pic''asso', '', 0, 'Au fond Ã  gauche', 70),
-(69, 'Support', '', 0, 'Si Ã§a ne marche pas, dÃ©merdez vous !', 0),
-(66, 'Pita party', '', 0, 'Le roi de la pita', 61),
-(67, 'RU', '', 0, 'Berk', 61),
-(68, 'CrÃ©dits', '', 0, 'Tit et Martin on beaucoup travaillÃ©', 0),
-(65, 'Kebab the best', '', 0, 'Miam', 61),
-(64, 'En voiture', '', 0, 'C''est pas dur', 62),
-(63, 'En train', '', 0, 'C''est facile', 62),
-(62, 'AccÃ¨s', '', 1, '', 0),
-(61, 'Restauration', '', 1, '', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -122,14 +86,6 @@ CREATE TABLE IF NOT EXISTS `news` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=36 ;
-
---
--- Contenu de la table `news`
---
-
-INSERT INTO `news` (`id`, `title`, `content`, `date`) VALUES
-(34, 'Lancement de l''application', 'L''application Imaginarium Festival est maintenant disponible sur les smartphones Android et iOS ! TÃ©lÃ©chargez la !', '2013-11-17 16:21:04'),
-(35, 'Nouvelle news', 'Une news plus rÃ©cente pour tester', '2013-11-18 15:44:01');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
