355b9dc9d75e389453f0c0c2d71cbe8b0e0861da
TitouanR <titouan.rossier@gmail.com>
2014-03-03 21:33:17 -0800

    Modify base dump for IF set up

--- base.sql
+++ base.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: mysql51-101.perso
--- Généré le : Lun 18 Novembre 2013 à 17:04
+-- Généré le : Mar 04 Mars 2014 à 06:14
 -- Version du serveur: 5.1.66
 -- Version de PHP: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `titouanrif`
@@ -23,10 +17,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `artists`
+-- Structure de la table `bo_artists`
 --
 
-CREATE TABLE IF NOT EXISTS `artists` (
+CREATE TABLE IF NOT EXISTS `bo_artists` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `picture` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
@@ -42,27 +36,26 @@ CREATE TABLE IF NOT EXISTS `artists` (
   `youtube` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=24 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
 
--- --------------------------------------------------------
 
 --
--- Structure de la table `filters`
+-- Structure de la table `bo_filters`
 --
 
-CREATE TABLE IF NOT EXISTS `filters` (
+CREATE TABLE IF NOT EXISTS `bo_filters` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `url` text NOT NULL,
+  `picture` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `infos`
+-- Structure de la table `bo_infos`
 --
 
-CREATE TABLE IF NOT EXISTS `infos` (
+CREATE TABLE IF NOT EXISTS `bo_infos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `picture` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -71,52 +64,47 @@ CREATE TABLE IF NOT EXISTS `infos` (
   `parent` int(11) NOT NULL,
   `isDisplayedOnMap` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=93 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `news`
---
-
-CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content` text COLLATE utf8_unicode_ci NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=36 ;
-
--- --------------------------------------------------------
 
 --
--- Structure de la table `partner`
+-- Structure de la table `bo_map`
 --
 
-CREATE TABLE IF NOT EXISTS `partner` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `picture` varchar(255) NOT NULL,
-  `weblink` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `map`
---
-
-CREATE TABLE IF NOT EXISTS `map` (
+CREATE TABLE IF NOT EXISTS `bo_map` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `x` float NOT NULL,
   `y` float NOT NULL,
   `infoId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bo_news`
+--
+
+CREATE TABLE IF NOT EXISTS `bo_news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+
+
+--
+-- Structure de la table `bo_partners`
+--
+
+CREATE TABLE IF NOT EXISTS `bo_partners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
