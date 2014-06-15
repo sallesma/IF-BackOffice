2ddd478788fc1e7c15a7ebfb3c3eed966cdf7dcf
Martin Salles <salles.martin@gmail.com>
2014-06-15 17:19:23 +0200

    Fix default params in js function for buttons

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,20 @@
-function progress (button, text='') {
+function progress (button, text) {
+	text = typeof text !== 'undefined' ? text : ''
+
 	button.attr("disabled", "disabled");
 	button.html("<i class='fa fa-refresh fa-spin'></i>" + text);
 }
 
-function remove (button, text='') {
+function remove (button, text) {
+	text = typeof text !== 'undefined' ? text : ''
+
 	button.removeAttr("disabled");
 	button.html("<i class='fa fa-times'></i>" + text);
 }
 
-function edit (button, text='') {
+function edit (button, text) {
+	text = typeof text !== 'undefined' ? text : ''
+
 	button.removeAttr("disabled");
 	button.html("<i class='fa fa-pencil'></i>" + text);
 }
